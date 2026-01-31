1919275a55044460deac72abee22ba6ba3fabe51
RimshaArfeen <rimshaarfeen61@gmail.com>
2026-01-31 21:14:51 +0500

    Update fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ConditionalLayout from "@/components/layout/ConditionalLayout";
+import { Work_Sans } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const workSans = Work_Sans({
   subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-work-sans",
 });
 
 const geistMono = Geist_Mono({
@@ -30,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased`}
+        className={`${workSans.variable} ${workSans.variable} min-h-screen antialiased`}
       >
         <Script
           src="/theme-init.js"
